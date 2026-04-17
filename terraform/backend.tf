db6a247db0bab7af9e26a0861ba3bfce78ca0acb
Soniya Malviya <151515611+soniya-malviy@users.noreply.github.com>
2026-04-17 23:14:57 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shopsmart-terraform-state"
+    bucket = "shopsmart-terraform-state-us"
     key    = "rubrics/terraform.tfstate"
     region = "us-east-1"
   }
