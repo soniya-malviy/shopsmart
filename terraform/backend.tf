681743275bb76b3db087ce65bf65dbfdd126a871
Soniya Malviya <151515611+soniya-malviy@users.noreply.github.com>
2026-04-18 01:33:06 +0530

    Rename S3 bucket for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "shopsmart-terraform-state-us"
+    bucket = "shopsmart-terraform-state"
     key    = "rubrics/terraform.tfstate"
     region = "us-east-1"
   }
