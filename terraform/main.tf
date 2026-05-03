1281c538e16a51b4ee279017a924b2bfdcc74c85
soniya-malviy <soniya.04malviya@gmail.com>
2026-05-03 21:33:44 +0530

    Add CloudWatch log group to Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,8 @@ resource "aws_s3_bucket_public_access_block" "app_bucket" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name              = "/ecs/shopsmart-backend"
+  retention_in_days = 7
+}
